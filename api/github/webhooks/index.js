c7be114b692a43b1a403b8ad268e13895ba4be5d
thilllon <thilllon970@gmail.com>
2024-05-27 18:41:50 +0900

    feat: add index.html

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,7 +1,9 @@
 import { createNodeMiddleware, createProbot } from 'probot';
 import { probotApp } from '../../../lib/app.js';
 
-export default createNodeMiddleware(probotApp, {
+const requestListener = createNodeMiddleware(probotApp, {
   probot: createProbot(),
   webhooksPath: '/api/github/webhooks',
 });
+
+export default requestListener;
