b73a47e971f8fc0e1024f0e3927376b909a070d9
thilllon <thilllon970@gmail.com>
2024-05-27 19:15:02 +0900

    feat: add tsconfig

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 export default async function handler(req, res) {
+  console.log(process.env.WEBHOOK_SECRET);
+  console.log(process.env.WEBHOOK_SECRET);
   res.status(200).json({ now: new Date().toISOString() });
 }
